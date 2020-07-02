9ca072ca89d2828f9348489198c1c5b25162eec9
ACZJ <aczj@edu.gpnu>
2020-07-03 00:07:10 +0800

    IDEA使用Git测试更新4

--- HelloGit/src/main/java/edu/gpnu/git/HelloGit.java
+++ HelloGit/src/main/java/edu/gpnu/git/HelloGit.java
@@ -3,10 +3,10 @@ package edu.gpnu.git;
 /**
  * @version 1.0
  * @Author:Cao Zhijian
- * @DATE:2020/7/2 0002 22:56
+ * @DATE:2020/7/3 0003 0:04
  */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Test!!");
     }
 }
